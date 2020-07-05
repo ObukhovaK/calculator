0bb83a55449c873497b9008c2d239b9160afa934
ObukhovaK <61306496+ObukhovaK@users.noreply.github.com>
2020-07-05 20:32:46 +0300

    File input/output!

--- calc_02.c
+++ calc_02.c
@@ -17,30 +17,37 @@ int main(void) {
     setvbuf(stdout, NULL, _IONBF, 0);
     setvbuf(stderr, NULL, _IONBF, 0);
 
+FILE *inFile, *outFile;
 int vl, i;                           /* vl - vector length */
 float a,b,result,count,pre;       /* a,b - numbers, count - counter, pre - preliminary result*/
-char oper, go, type;                          /*oper - operand, go - starts/restarts calculator,
+char oper, go, type;                /*oper - operand, go - starts/restarts calculator,
                                                 type - chooses vector of numeral operations   */
 float *v1, *v2;                   /* vectors */
+char nameIn[20], nameOut[20];
     pre = 1;
 
-
-    printf("Press 'g' to start");
-    scanf(" %c", &go);
-    if (go == 'g') {
-    printf("for numbers operations press 'n', for vectors press 'v'");
-    scanf(" %c", &type);
+printf("Press 'g' to start");
+scanf(" %c", &go);
+while (go == 'g') {
+    inFile = NULL;
+    while (inFile == NULL) {          /* opens files, prints "error" if the input file doesn't exist*/
+    printf("Enter input file name (20 characters or less):");
+    scanf(" %s", &nameIn);
+    inFile = fopen(nameIn, "r");
+    if (inFile == NULL){
+            printf("This file does not exist.\n");
+        } else {
+            printf("Enter output file name (20 characters or less): ");
+            scanf(" %s", nameOut);
+            outFile = fopen(nameOut, "w");
+        }
+    }
+    fscanf(inFile, " %c", &oper);
+    fscanf(inFile, " %c", &type);
 
 if (type == 'n') {          // start numbers operation block
-
-    printf("Enter a, operand, b. \n");
-    printf(" Acceptable operands: +, -, *, /, ^, ! \n");
-    printf(" Notice: for exponentiation, b must be integer and more than 1  \n");
-    printf(" Notice: for factorial, a must be integer and more than 1  \n");
-
-    scanf(" %f",&a);
-    scanf(" %c",&oper);
-    scanf(" %f",&b);
+    fscanf(inFile, " %f", &a);
+    fscanf(inFile, " %f", &b);
 
 switch (oper) {
     case'+':                /* finds a+b */
@@ -54,7 +61,7 @@ switch (oper) {
         break;
     case'/':                /* finds a/b */
        if(b != 0) result = a / b;
-       else puts("error: b = 0");
+       else fprintf(outFile, "error: b = 0");
        break;
     case'^':        /* finds а^b, if b < 1 prints "error" */
         count = 0;
@@ -64,7 +71,7 @@ switch (oper) {
             pre = pre*a;
         }
     result = pre; }
-        else puts("error");
+        else fprintf(outFile,"error");
     break;
     case'!':                /* finds а factorial, if а < 1  prints "error" */
         count = 1;
@@ -75,53 +82,65 @@ switch (oper) {
         }
         result = pre;
         }
-        else puts("error");
+        else fprintf(outFile,"error");
     break;
     }
-        puts(" = ");
-        printf(" %f",result);
+        fprintf(outFile, " %f %c %f = ", a, oper, b);
+        fprintf(outFile, " %f\n",result);
         } // end numbers operations block
 
 else if (type == 'v') {     // start vector operations block
-            printf("Enter vectors length ");
-            scanf(" %i", &vl);
+            fscanf(inFile, " %i", &vl);
             v1 = malloc(vl*sizeof(int));
             v2 = malloc(vl*sizeof(int));
-
-            printf("Enter vector 1 \n");
-                for (i = 0; i < vl; i++) {
-                    scanf(" %f", &v1[i]);
+            fprintf(outFile,"( ");
+            for (i = 0; i < vl; i++) {    // reads vectors and operation, puts them into output file
+                    fscanf(inFile," %f", &v1[i]);
+                    fprintf(outFile," %f", v1[i]);
                 }
-            printf("Enter vector 2 \n");
+            fprintf(outFile," ) ");
+            fprintf(outFile," %c", oper);
+            fprintf(outFile," ( ");
             for (i = 0; i < vl; i++) {
-                scanf(" %f", &v2[i]);
+                fscanf(inFile," %f", &v2[i]);
+                fprintf(outFile," %f", v2[i]);
                }
-            printf("Enter operand \n");
-            printf(" Acceptable operands: +, -, * \n");
-            scanf(" %c", &oper);
-
+            fprintf(outFile," )");
  switch(oper) {
- case'+':                           /* finds (v1 + v2) */
+ case'+':                           // finds (v1 + v2)
+     fprintf(outFile,"( ");
      for (i = 0; i < vl; i++) {
-         printf("%f ", v1[i] + v2[i]);
+         fprintf(outFile, "%f ", v1[i] + v2[i]);
      }
+     fprintf(outFile," )\n");
      break;
- case'-':                           /* finds (v1 - v2) */
+ case'-':                           // finds (v1 - v2)
+     fprintf(outFile,"( ");
      for (i = 0; i < vl; i++) {
-         printf("%f ", v1[i] - v2[i]);
+         fprintf(outFile, "%f ", v1[i] - v2[i]);
      }
+     fprintf(outFile," )\n");
      break;
  case'*':
+     fprintf(outFile,"( ");
       for (i = 0; i < vl; i++) {
-          printf("%f ", v1[i] * v2[i]);  /* finds (v1 * v2) */
+          fprintf(outFile, "%f ", v1[i] * v2[i]);  // finds (v1 * v2)
      }
+      fprintf(outFile," )\n");
      break;
  }   // end switch case
-        } // end vectors operations block
-
+ free(v1);
+ free(v2);
+} // end vectors operations block
+else {
+printf("Input file is incorrect. Please try again.");
 }
-free(v1);
-free(v2);
+
+fclose(inFile);
+fclose(outFile);
+printf("Press 'g' to restart. Press any other key to exit");
+}
+
     return EXIT_SUCCESS;
 }
 
